3293254af14bbec98187c8d0d6638f12361ccfe7
PhilipeF <philipefdev@gmail.com>
2025-01-22 00:07:44 -0300

    Style: Dando início a estilização do modal de editar tarefa

--- src/pages/TaskList/styles.ts
+++ src/pages/TaskList/styles.ts
@@ -80,3 +80,15 @@ export const BtnCreateNewTask = styled.button`
     background-color: var(--blue-light);
   }
 `;
+
+export const Modal = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
